493ddd533329f033673bf82398ead169648faf8e
Rob Allen <rob.allen@iamcloud.com>
2021-11-22 10:24:05 +0000

    add git clone to VM

---
+++ vagrant-install-test/ps1/Get-AzureProject.ps1
@@ -0,0 +1,17 @@
+<#
+.SYNOPSIS
+    SYNOPSIS
+.DESCRIPTION
+    DESCRIPTION
+.NOTES
+    NOTES
+.INPUTS
+    N/A
+.OUTPUTS
+    N/A
+.EXAMPLE
+#>
+
+$thePath = "c:\"
+Set-Location  -Path $thePath
+git clone https://github.com/TheMasterPrawn/parsec-azure.git

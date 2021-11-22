6d83135fd238db327ee3ddba361a04aae4d134e0
Rob Allen <robertallen@outlook.com>
2021-11-22 11:30:36 +0000

    change folder structure

---
+++ Setup-Parsec.ps1
@@ -0,0 +1,20 @@
+
+<#
+.SYNOPSIS
+    SYNOPSIS
+.DESCRIPTION
+    DESCRIPTION
+.NOTES
+    NOTES
+.INPUTS
+    N/A
+.OUTPUTS
+    N/A
+.EXAMPLE
+#>
+
+$installers = @('steam')
+$psscriptroot
+.\Get-GameInstallers.ps1 -installers $installers
+
+.\Disable-Updates.ps1
